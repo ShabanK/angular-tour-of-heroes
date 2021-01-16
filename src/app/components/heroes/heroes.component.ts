07093865286ba481f6ad6f209269328e2433f62c
ShabanK <shabankhawar99@gmail.com>
2021-01-17 02:41:45 +0500

    used ngModel and *ngFor

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../models/hero';
+import { HEROES } from '../../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero = {
+    id: 1,
+    name: 'Philza',
+  };
+  heroes: Hero[] = HEROES;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
